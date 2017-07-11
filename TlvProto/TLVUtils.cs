4acab40afdf7a687b4bcb60ebfe5a6391fc437d5
Ams <adriansims62781@gmail.com>
2017-07-11 01:21:27 +0100

    first commit

---
+++ TlvProto/TLVUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+namespace TlvProto
+{
+    internal partial class Program
+    {
+        static class TLVUtils
+        {
+            public static int Add<T>(MemoryStream ms, T i)
+            {
+                return 99999;
+            }
+        }
+    }
+}
