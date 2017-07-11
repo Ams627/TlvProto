504970127f49d125087e257511b8c77af8e447b6
Ams <adriansims62781@gmail.com>
2017-07-11 01:19:29 +0100

    First commit

---
+++ TlvProto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TlvProto
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var m = new MemoryStream();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
